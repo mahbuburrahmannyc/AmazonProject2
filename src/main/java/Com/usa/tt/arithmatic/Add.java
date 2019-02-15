335c44cdbc7493456213d1cba5b4f40fba54ebb0
mdhaque <mdhaque@mds-macbook-pro.fios-router.home>
2019-02-15 16:53:51 -0500

    this project

---
+++ src/main/java/Com/usa/tt/arithmatic/Add.java
@@ -0,0 +1,34 @@
+package Com.usa.tt.arithmatic;
+
+public class Add {
+	
+	public static void main(String[] args) {
+	 
+		int a;//Declaration
+		a=10;//initialization
+		int b;
+		b=20; 
+		int c=a+b;
+		System.out.println("This is addition : " +c);
+		
+		int x=20;
+		int y=30;
+		int z=y-x;
+		
+		if (a>b){System.out.println("A is Bigger Number"); }
+		
+		System.out.println("this is Subtraction : " +z);
+		
+		if (a>b){
+			System.out.println("A is Bigger Number"); 
+		}else
+			System.out.println("A is smaller Number");
+
+		
+			
+			
+	
+	}//main method
+	
+
+}//end of the class
